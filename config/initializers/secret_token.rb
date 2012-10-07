953604ca7535d791969bd12d61ed97e43f1206ba
RD99 <sakkiraju@hotmail.com>
2012-10-06 21:43:17 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blitter::Application.config.secret_token = 'cd8348e54bc83a1999a9002c2d05699cba4a8647ac6a0460269fb2c3552228ebb4e685c65f7b0c5a391c145a1a26ce11c5bfe756013b30bd5522376e1ac0f0c9'
